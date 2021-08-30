6b6491bd374e266dedffba08b2a418f6c2ccfd11
Dragan Pepic <d.pepic@hotmail.com>
2021-08-30 15:38:48 +0200

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = a;
+            b++;
+            Console.WriteLine($"a je {a} a b je {b}");
+
+            Neka ao = new();
+            ao.broj = 5;
+            Neka bo = ao;
+            bo.broj++;
+            ao.broj++;
+            Console.Write($"a je {ao.broj} a b je {bo.broj}");
+        }
+    }
+    class Neka
+    {
+        public int broj;
+    }
+
+}
